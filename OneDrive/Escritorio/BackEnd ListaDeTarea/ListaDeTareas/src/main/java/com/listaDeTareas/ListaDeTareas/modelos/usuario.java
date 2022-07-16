602ad97091875e8525c4e80e42033fa04d352a9c
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2022-07-16 03:11:12 -0300

    entidad usuario terminada

--- OneDrive/Escritorio/BackEnd ListaDeTarea/ListaDeTareas/src/main/java/com/listaDeTareas/ListaDeTareas/modelos/usuario.java
+++ OneDrive/Escritorio/BackEnd ListaDeTarea/ListaDeTareas/src/main/java/com/listaDeTareas/ListaDeTareas/modelos/usuario.java
@@ -1,10 +1,125 @@
 package com.listaDeTareas.ListaDeTareas.modelos;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  *
  * @author nicolas lans
  */
+@Getter @Setter
 @Entity
 public class usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    private String nombre;
+    private String fotoDePerfil;
+    private String banner;
+    @NotNull
+    @Column(unique = true)
+    private String nombreUsuario;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "usuario_id"),
+    inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<Rol> roles = new HashSet<>();
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "id")
+    public List<tareas> tareasList;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idCWeb")
+    public List<cambiosWeb> cambiosWebList;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idAGA")
+    public List<analisisGA> analisisGAList;
+    
+    
+
+    public Usuario() {
+    }
+
+
+    public usuario(@NotNull String nombre, String fotoDePerfil,String banner,@NotNull String nombreUsuario,@NotNull String email,@NotNull String password) {
+        
+        this.nombre = nombre;
+        this.fotoDePerfil = fotoDePerfil;
+        this.banner = banner;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getBanner() {
+        return banner;
+    }
+
+    public void setBanner(String banner) {
+        this.banner = banner;
+    }
+
+    public String getFotoDePerfil() {
+        return fotoDePerfil;
+    }
+
+    public void setFotoDePerfil(String fotoDePerfil) {
+        this.fotoDePerfil = fotoDePerfil;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Rol> roles) {
+        this.roles = roles;
+    }
 }
