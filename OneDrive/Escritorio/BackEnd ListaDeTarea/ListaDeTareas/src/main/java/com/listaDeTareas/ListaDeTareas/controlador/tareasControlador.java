94db8d166928bafb9530396027652ff0924a6db5
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2022-07-16 16:10:04 -0300

    agregue tares mvc y rol y rol nombre para el usuario

--- OneDrive/Escritorio/BackEnd ListaDeTarea/ListaDeTareas/src/main/java/com/listaDeTareas/ListaDeTareas/controlador/tareasControlador.java
+++ OneDrive/Escritorio/BackEnd ListaDeTarea/ListaDeTareas/src/main/java/com/listaDeTareas/ListaDeTareas/controlador/tareasControlador.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.listaDeTareas.ListaDeTareas.controlador;
+
+import com.listaDeTareas.ListaDeTareas.modelos.tareas;
+import com.listaDeTareas.ListaDeTareas.modelos.usuario;
+import com.listaDeTareas.ListaDeTareas.servicios.tareasServicio;
+import com.listaDeTareas.ListaDeTareas.servicios.usuarioServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
- * @author nicol
+ * @author nicolas Lans
  */
+@RestController
+@RequestMapping("/tareas")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class tareasControlador {
+    @Autowired
+    private tareasServicio tareasServ;
     
+    
+    @PostMapping ("/nuevo")
+    public void agregarTarea (@RequestBody tareas tarea){
+        tareasServ.addTareas(tarea);
+    }
+    
+    @GetMapping ("/ver")
+    @ResponseBody
+    public List<tareas> verTareas(){
+        return tareasServ.buscarTareas();
+    }
+    
+    
+    @DeleteMapping ("/delete/{id}")
+    public void borrarTarea (@PathVariable Long id){
+        tareasServ.borrarTareas(id);
+    }
+    
+    
+    @PutMapping("/actualizar")
+    public void actualizar (@RequestBody tareas tarea){
+        tareasServ.addTareas(tarea);
+    }
 }
--- OneDrive/Escritorio/BackEnd ListaDeTarea/ListaDeTareas/src/main/java/com/listaDeTareas/ListaDeTareas/seguridad/modelos/Rol.java
+++ OneDrive/Escritorio/BackEnd ListaDeTarea/ListaDeTareas/src/main/java/com/listaDeTareas/ListaDeTareas/seguridad/modelos/Rol.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.listaDeTareas.ListaDeTareas.seguridad.modelos;
+
+import com.listaDeTareas.ListaDeTareas.seguridad.modelos.enums.RolNombre;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 /**
  *
- * @author nicol
+ * @author nicolas Lans
  */
+@Entity
 public class Rol {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+
+    public Rol() {
+    }
+
+    public Rol(@NotNull RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
 }
