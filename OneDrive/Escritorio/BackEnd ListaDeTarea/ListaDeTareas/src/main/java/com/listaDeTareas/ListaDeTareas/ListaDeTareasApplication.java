e683236244225990b427a8d6e21248360419ce28
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2022-07-16 02:11:10 -0300

    desarrollo de backend lista de tareas para fauna

---
+++ OneDrive/Escritorio/BackEnd ListaDeTarea/ListaDeTareas/src/main/java/com/listaDeTareas/ListaDeTareas/ListaDeTareasApplication.java
@@ -0,0 +1,13 @@
+package com.listaDeTareas.ListaDeTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeTareasApplication.class, args);
+	}
+
+}
