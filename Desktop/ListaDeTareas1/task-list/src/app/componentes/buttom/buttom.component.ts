005263fd88121b183fa9d2a5264739de19fd0a91
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-04-18 23:19:03 -0300

    primer boton

---
+++ Desktop/ListaDeTareas1/task-list/src/app/componentes/buttom/buttom.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, OnInit , Output} from '@angular/core';
+
+@Component({
+  selector: 'app-buttom',
+  templateUrl: './buttom.component.html',
+  styleUrls: ['./buttom.component.css']
+})
+export class ButtomComponent implements OnInit {
+  @Input() text: string= ""
+  @Input() color: string= "";
+  @Output() btnClick = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    this.btnClick.emit();
+    console.log("click")
+
+  }
+}
